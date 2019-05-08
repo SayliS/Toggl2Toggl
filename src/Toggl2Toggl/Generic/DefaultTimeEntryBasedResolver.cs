8b89932db6db520ad8e150d63bcf897f3902bfda
SayliS <kalin.venkov@eldersoss.com>
2019-05-08 11:54:53 +0300

    Adds DefaultTimeEntryBasedResolver for cases where we need to fall back

---
+++ src/Toggl2Toggl/Generic/DefaultTimeEntryBasedResolver.cs
@@ -0,0 +1,23 @@
+﻿using Toggl;
+
+namespace Toggl2Toggl
+{
+    public class DefaultTimeEntryBasedResolver<T> : ITimeEntryBasedResolver<T>
+    {
+        readonly T defaultEntry;
+
+        public DefaultTimeEntryBasedResolver(T defaultEntry, int weight)
+        {
+            this.defaultEntry = defaultEntry;
+            Weight = weight;
+        }
+
+        public int Weight { get; private set; }
+
+        public bool TryResolve(TimeEntry entry, out T result)
+        {
+            result = defaultEntry;
+            return true;
+        }
+    }
+}
